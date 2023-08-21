0c4569570a27bc63ecd272c1fadc467f16546cb6
Venkatesh <54614167+venkatesh547@users.noreply.github.com>
2023-08-21 23:52:37 +0530

    Update spark_optimise.py

--- Nallas_spark/spark_optimise.py
+++ Nallas_spark/spark_optimise.py
@@ -12,20 +12,17 @@
 
 #Sample Queries:
  select count(*) from employee WHERE joining_date < 2023
- SELECT count(*) FROM Employee WHERE Name = 'prem'
- SELECT * FROM Employee WHERE Name ='Dennis' AND Sal=25000 AND Experience=5
- SELECT * FROM Employee WHERE Sal>18000
- SELECT * FROM Employee WHERE Experience>5
+ SELECT count(*) from Employee WHERE Name = 'prem'
+ SELECT * from Employee WHERE Name ='Dennis' AND Sal=25000 AND Experience=5
+ SELECT * from Employee WHERE Sal>18000
+ SELECT * from Employee WHERE Experience>5
 
 
 
 
- We can use Indexing and Z-order(data-skipping algorithms) to dramatically reduce the amount of data that needs to be read.
+ # We can use Indexing and Z-order(data-skipping algorithms) to dramatically reduce the amount of data that needs to be read.
 
- Data shuffle won't happen in z-order, it only co-locate the data stats range and logs it in the 'json' file under the Delta log folder.
-
-
- code::
+ # Data shuffle won't happen in z-order, it only co-locate the data stats range and logs it in the 'json' file under the Delta log folder.
 
  # repartitionByRange will group the data in different partitions.
  # if we are filtering mostly by a single column('id'), then repartitionByRange works well. But, in our case Z-order helps since we are going to use multiple filter columns
